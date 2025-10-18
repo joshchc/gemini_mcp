2b42477ed78d51fdc3f0b0420b466de6054a2110
joshchc <asdfghjk1108011@gmail.com>
2025-10-18 23:08:03 +0800

    fix: remove personal api

--- check_models.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key = 'AIzaSyDsSAvfkANfOrQTawrm_m2Wg3SRwdyoldk')
-
-print("可用的模型：")
-for model in genai.list_models():
-    if 'generateContent' in model.supported_generation_methods:
-        print(f"- {model.name}")
